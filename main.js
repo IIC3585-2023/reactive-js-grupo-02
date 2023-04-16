4844360129cf4a612f413a5c2df00ef67c7c5c1e
fbesoainp <chachibesoain1@gmail.com>
2023-04-16 17:38:16 -0400

    ✨ Add sprites movement

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
-import { players } from './scripts/player';
+import { playersData } from './scripts/player';
 import { fromEvent } from 'rxjs';
 import { filter } from 'rxjs/operators';
 
 const grid = [
-    [".",".",".",".","x",".",".",".",".",".",".",".",".","x",".",".",".","."],
-    [".","x","x",".","x",".","x","x","x","x","x","x",".","x",".","x","x","."],
-    [".","x",".",".",".",".",".",".",".",".",".",".",".",".",".",".","x","."],
-    [".","x",".","x","x",".","x","x",".",".","x","x",".","x","x",".","x","."],
-    [".",".",".",".",".",".","x",".",".",".",".","x",".",".",".",".",".","."],
-    [".","x",".","x","x",".","x","x","x","x","x","x",".","x","x",".","x","."],
-    [".","x",".",".",".",".",".",".",".",".",".",".",".",".",".",".","x","."],
-    [".","x","x",".","x",".","x","x","x","x","x","x",".","x",".","x","x","."],
-    [".",".",".",".","x",".",".",".",".",".",".",".",".","x",".",".",".","."]
+    [" ","*","*","*","X","*","*","*","*","*","*","*","*","X","*","*","*"," "],
+    ["*","X","X","*","X","*","X","X","X","X","X","X","*","X","*","X","X","*"],
+    ["*","X","*","*","*","*","*","*","*","*","*","*","*","*","*","*","X","*"],
+    ["*","X","*","X","X","*","X","X"," "," ","X","X","*","X","X","*","X","*"],
+    ["*","*","*","*","*","*","X"," "," "," "," ","X","*","*","*","*","*","*"],
+    ["*","X","*","X","X","*","X","X","X","X","X","X","*","X","X","*","X","*"],
+    ["*","X","*","*","*","*","*","*","*","*","*","*","*","*","*","*","X","*"],
+    ["*","X","X","*","X","*","X","X","X","X","X","X","*","X","*","X","X","*"],
+    ["*","*","*","*","X","*","*","*","*","*","*","*","*","X","*","*","*"," "]
 ];
 
+let players = playersData
 const canvas = document.getElementById("map");
 const ctx = canvas.getContext("2d");
 
@@ -25,41 +26,111 @@ const getXPixPos = (x) => x * POSWIDTH;
 const getYPixPos = (y) => y * POSHEIGHT;
 
 const drawPlayer = (player) => {
-    const sprite = new Image(30, 30);
-    sprite.src = player.sprite;
-    console.log(player.x, player.y);
-    sprite.onload = function () {
-        const x = getXPixPos(player.x) + 10;
-        const y = getYPixPos(player.y) + 10;
-        ctx.drawImage(sprite, x, y, 30, 30);
-    };
+  const sprite = new Image(30, 30);
+  sprite.src = player.sprite;
+  console.log(player.x, player.y);
+  sprite.onload = function () {
+      const x = getXPixPos(player.x) + 10;
+      const y = getYPixPos(player.y) + 10;
+      ctx.drawImage(sprite, x, y, 30, 30);
+  };
 };
+
+const drawPoints = () => {
+  const sprite = new Image(30, 30);
+  sprite.src = "point.svg";
+  sprite.onload = function () {
+    grid.forEach((row, y) => {
+      row.forEach((point, x) => {
+        if (point === "*") {
+            ctx.drawImage(sprite, getXPixPos(x) + 10, getYPixPos(y) + 10, 20, 20);
+          };
+      });
+    });
+  };
+};
+
 
 const updateGrid = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawPoints();
     players.forEach((player) => drawPlayer(player));
+    setTimeout(() => {
+      checkPoints();
+      checkDeath();
+    }, 100);
 };
-
+  
 const movePlayer = (x, y, player) => {
-    const posX = player.x + 1 * x;
-    const posY = player.y + 1 * y;
-
-  if (grid[posY][posX]=== "x" || grid[posY][posX]=== undefined || posX < 0 || posY > 8 || posX < 0 && posY > 17) {
+  const posX = player.x + 1 * x;
+  const posY = player.y + 1 * y;
+    
+  if (grid[posY][posX]=== "X" || grid[posY][posX]=== undefined || posX < 0 || posY > 8 || posX < 0 && posY > 17) {
     return
   }
+
+  if (grid[posY][posX] === "*" && !player.isEnemy) {
+    grid[posY][posX] = " ";
+  }
+
+  if ( x === -1 && y === 0 ) {
+    player.sprite = `${player.path}/left.png`;
+  }
+  if ( x === 1 && y === 0 ) {
+    player.sprite = `${player.path}/right.png`;
+  }
+  if ( x === 0 && y === -1 ) {
+    player.sprite = `${player.path}/up.png`;
+  }
+  if ( x === 0 && y === 1 ) {
+    player.sprite = `${player.path}/down.png`;
+  }
+
   player.x  = posX;
   player.y = posY;
   updateGrid();
 };
 
-const RegisterInput = (control, player) => {
-    const keyPress = fromEvent(document, 'keydown').pipe(
-      filter((event) => event.key === control.key),
-    );
-    keyPress.subscribe(() => {
-      movePlayer(control.x,control.y, player);
-    }); 
+const checkPoints = () => {
+  const isPointsLeft = (map) => {
+    let points = 0;
+    for (let i = 0; i < map.length; i++) {
+      for (let j = 0; j < map[i].length; j++) {
+        if (map[i][j] === "*") {
+          points++;
+        }
+      }
+    }
+    return points > 0;
+  };
+  if (!isPointsLeft(grid)) {
+    alert("Players win!");
+    location.reload();
   }
+};
+const checkDeath = () => {
+  console.log(players);
+  const enemies = players.filter((player) => player.isEnemy);
+  const allies = players.filter((player) => !player.isEnemy);
+  allies.forEach((ally) => {
+    if (enemies.some((enemy) => enemy.x === ally.x && enemy.y === ally.y)) {
+      players = players.filter((player) => player.id !== ally.id);
+    }
+  });
+  if (players.length === 1) {
+    alert(`Ghost wins!`);
+    location.reload();
+  }
+}
+
+const RegisterInput = (control, player) => {
+  const keyPress = fromEvent(document, 'keydown').pipe(
+    filter((event) => event.key === control.key),
+  );
+  keyPress.subscribe(() => {
+    movePlayer(control.x,control.y, player);
+  }); 
+}
   
 players.forEach((player) =>
   player.input.forEach((control) => RegisterInput(control, player))
--- scripts/player.js
+++ scripts/player.js
@@ -1,26 +1,58 @@
 const player1 = {
-    sprite: "player.svg",
+    id: 1,
+    path: "player-1",
+    sprite: "player-1/right.png",
     x: 0,
     y: 0,
     input: [
-        {key:'ArrowDown', x:0, y:1}, 
         {key:'ArrowUp', x:0, y:-1}, 
+        {key:'ArrowDown', x:0, y:1}, 
         {key:'ArrowLeft', x:-1, y:0}, 
         {key:'ArrowRight', x:1, y:0}
     ],
 };
 
 const player2 = {
-    sprite: "ghost.png",
+    id: 2,
+    path: "player-2",
+    sprite: "player-2/right.png",
+    x: 17,
+    y: 8,
+    input: [
+        {key:'i', x:0, y:-1}, 
+        {key:'k', x:0, y:1}, 
+        {key:'j', x:-1, y:0}, 
+        {key:'l', x:1, y:0}
+    ],
+};
+
+const player3 = {
+    id: 3,
+    path: "player-3",
+    sprite: "player-3/right.png",
+    x: 17,
+    y: 0,
+    input: [
+        {key:'t', x:0, y:-1}, 
+        {key:'g', x:0, y:1}, 
+        {key:'f', x:-1, y:0}, 
+        {key:'h', x:1, y:0}
+    ],
+};
+
+const player4 = {
+    id: 4,
+    path: "player-4",
+    sprite: "player-4/right.png",
     x: 9,
     y: 4,
     isEnemy: true,
     input: [
-        {key:'s', x:0, y:1}, 
         {key:'w', x:0, y:-1}, 
+        {key:'s', x:0, y:1}, 
         {key:'a', x:-1, y:0}, 
         {key:'d', x:1, y:0}
     ],
 };
 
-export const players = [player1, player2];
+export const playersData = [player1, player2, player3, player4];
